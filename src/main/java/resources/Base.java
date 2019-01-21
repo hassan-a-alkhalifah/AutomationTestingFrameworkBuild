9b63e1ac14d291cad2c45ed0da7c1cf0c5b593e5
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-21 12:49:30 -0800

    set up screenshot capabilities

--- src/main/java/resources/Base.java
+++ src/main/java/resources/Base.java
@@ -1,10 +1,14 @@
 package resources;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -28,6 +32,11 @@ public class Base {
 		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
+	}
+	
+	public void getScreenshot(String resultName) throws IOException {
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\zzmar_000\\Desktop\\IT_Related\\AutomationTestingFrameworkBuild\\testScreenshots\\screenshot-" + resultName + ".png"));
 	}
 	
 }
